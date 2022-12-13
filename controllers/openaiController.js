e2d3294fbbcd2fde45c68494bf3bb1e11bfcf63f
gyanaranjans <116304077+gyanaranjans@users.noreply.github.com>
2022-12-13 11:08:25 +0530

    Update openaiController.js

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require('openai');
 
 const configuration = new Configuration({
-  apiKey: process.example.env.OPENAI_API_KEY,
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
